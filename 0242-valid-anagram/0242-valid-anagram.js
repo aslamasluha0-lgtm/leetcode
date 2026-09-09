e39cc8bdf47fbe73ee164779fe48e450b9f4eb42
muhammed aslam <aslamasluha0@gmail.com>
2026-09-09 18:53:51 +0530

    Time: 28 ms (17.45%), Space: 58.9 MB (33.21%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,10 @@
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    let sortedS = s.split("").sort().join("");
+    let sortedT = t.split("").sort().join("");
+
+    return sortedS === sortedT;
+};
