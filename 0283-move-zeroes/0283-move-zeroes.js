5848f407e50338f9f15bb89d12f9a5012d410d40
muhammed aslam <aslamasluha0@gmail.com>
2026-09-10 10:27:47 +0530

    Time: 5 ms (21.14%), Space: 60.6 MB (36.52%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,10 @@
+
+var moveZeroes = function(nums) {
+  let j = 0;
+  for (i=0;i<nums.length;i++){
+    if(nums[i]!==0){
+        [nums[i],nums[j]]=[nums[j],nums[i]];
+        j ++;
+    }
+  }  
+};
